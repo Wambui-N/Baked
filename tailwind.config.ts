bdf5c17203f15f686d47e28a54398f9543ad96f2
Wambui Ndungu <wambuin18@gmail.com>
2024-04-05 11:03:42 +0300

    prep for new site

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'transparent': 'transparent',
+        'primary': '#3F37D2',
+        'background': '#EFEFFA',
+        'black': '#090722',
+      }
     },
   },
   plugins: [],
